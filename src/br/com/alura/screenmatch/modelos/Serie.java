03e0a8e538702fa3464b22afb7674e9cb8222abd
gabriela rodrigues <gabriela.rodriguessilva@outlook.com>
2026-02-27 17:25:52 -0300

    Atualização projeto academico - Alura

--- src/br/com/alura/screenmatch/modelos/Serie.java
+++ src/br/com/alura/screenmatch/modelos/Serie.java
@@ -47,4 +47,8 @@ public class Serie extends Titulo {
     public int getDuracaoEmMinutos() {
         return temporadas * episodiosPorTemporada * minutosPorEpisodio;
     }
+    @Override
+    public String toString(){
+        return "Série: " + this.getNome() + "(" + this.getAnoDeLancamento() + ")";
+    }
 }
--- src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -3,7 +3,7 @@ import br.com.alura.screenmatch.modelos.Titulo;
 import br.com.alura.screenmatch.modelos.Serie;
 import br.com.alura.screenmatch.modelos.Filme;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
@@ -17,17 +17,32 @@ public class PrincipalComListas {
 
         Filme f1 = filmeDoPaulo;
 
-        ArrayList<Titulo> lista = new ArrayList<>();
+        List<Titulo> lista = new LinkedList<>();
         lista.add(filmeDoPaulo);
         lista.add(meuFilme);
         lista.add(outroFilme);
         lista.add(lost);
-
         for (Titulo item : lista) {
             System.out.println(item.getNome());
             if (item instanceof Filme filme && filme.getClassificacao() > 2) {
                 System.out.println("Classificação " + filme.getClassificacao());
             }
+
+            ArrayList<String> buscaPorArtista = new ArrayList<>();
+            buscaPorArtista.add("Adam Sandler");
+            buscaPorArtista.add("Paulo");
+            buscaPorArtista.add("Jaqueline");
+            System.out.println(buscaPorArtista);
+
+            Collections.sort(buscaPorArtista);
+            System.out.println("Depois da ordenação");
+            System.out.println(buscaPorArtista);
+            System.out.println("Lista de títulos ordenados");
+            Collections.sort(lista);
+            System.out.println(lista);
+            lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+            System.out.println("Ordenando por ano");
+            System.out.println(lista);
 
         }
     }
